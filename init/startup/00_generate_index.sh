286cd850e889130ebc808bb816f15d8447ade609
Christine Dodrill <xena@yolo-swag.com>
2015-02-14 09:11:33 -0800

    fix the index generation

--- init/startup/00_generate_index.sh
+++ init/startup/00_generate_index.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+cd /tmp
+wget http://www.frykholm.se/files/markdown.lua
+
 cd /app/src/blog
 
 IFS='
@@ -10,9 +13,9 @@ IFS='
 for file in $(ls -t)
 do
 	title=$(head -n1 $file)
-	line=$(head -n4 $file | tail -n1)
+	line=$(head -n4 $file | tail -n1 | lua /tmp/markdown.lua -n)
 	linkname=$(echo $file | awk -F "." '{print $1}')
 
-	echo '<div class="post"><h2>'"$title"'</h2><p>'"$line"'...</p><a href="/blog/'"$linkname"'">Read More</a></div>' >> /app/src/static/markdown/blog.html
+	echo '<div class="post"><h2>'"$title"'</h2>'"$line"'<a href="/blog/'"$linkname"'">Read More</a></div>' >> /app/src/static/markdown/blog.html
 	echo "generated info for $file"
 done
